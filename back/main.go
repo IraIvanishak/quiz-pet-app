735b0cce8d43cd7ee012374273d058520c19b1c4
IraIvanishak <iraivanishakcont@gmail.com>
2023-10-05 13:46:26 +0300

    Order code.

--- back/main.go
+++ back/main.go
@@ -13,21 +13,18 @@ var portN = ":8080"
 
 func main() {
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://127.0.0.1:5500"}, // Specify the allowed origin here
+		AllowedOrigins:   []string{"http://127.0.0.1:5500"},
 		AllowedMethods:   []string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
 	})
-
 	defer config.DB.Close()
 
 	r := chi.NewRouter()
 	r.Use(c.Handler)
-
 	r.Get("/", getAllTestPreview)
 	r.Post("/test", getTestResult)
 	r.Get("/test", getTestByID)
-	r.Get("/test-res", getTestByID)
 
 	fmt.Printf("Server started on port %s\n", portN)
 	err := http.ListenAndServe(portN, r)
